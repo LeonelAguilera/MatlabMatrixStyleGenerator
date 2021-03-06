e9983b78ca270e5fa010494462ded622c2d448f0
LeonelAguilera <64066370+LeonelAguilera@users.noreply.github.com>
2021-03-06 21:16:24 +0100

    Color and char consonance
    Now color and characters wont rewirte in every frame

--- MatrixatorAnim.m
+++ MatrixatorAnim.m
@@ -40,7 +40,7 @@ output = hsv2rgb(output);
 imshow(output)
 pause(0.1);
 
-for frame = 2:10
+for frame = 2:60
     lineStart = Index*step-frame*step;
     lineEnd   = lineStart + step*(ResY-1);
     
@@ -49,15 +49,18 @@ for frame = 2:10
     end
     ShadowMap = ShadowMap-floor(ShadowMap);
     
-    letters = round((126-33)*rand(ResY,ResX))+33;
+    letters(2:ResY,:) = letters(1:ResY-1,:);
+    letters(1,:) = round((126-33)*rand(1,ResX))+33;
     
     CharMap = [characters{letters(1,:)}];
     for y = 2:ResY
         CharMap = [CharMap;[characters{letters(y,:)}]];
     end
     
-    H = rand(ResY,ResX)*(colorB(1)-colorA(1))+colorA(1);
-    S = rand(ResY,ResX)*(colorB(2)-colorA(2))+colorA(2);
+    H(2:ResY,:) = H(1:ResY-1,:);
+    H(1,:) = rand(1,ResX)*(colorB(1)-colorA(1))+colorA(1);
+    S(2:ResY,:) = S(1:ResY-1,:);
+    S(1,:) = rand(1,ResX)*(colorB(2)-colorA(2))+colorA(2);
     V = kron(ShadowMap,ones(9,7));
     V = V.*CharMap;
     
