db20c1670c918a3cbe2861be2b2ff7605aa40acf
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-03-07 22:52:21 +0100

    Fixed relative path in CharLoader and translated into comments

--- CharLoader.m
+++ CharLoader.m
@@ -1,8 +1,9 @@
 function characters = CharLoader()
+% Loads all characters in folder Caracteres into a cell column vector
 characters = cell(1,128-33);
 for i = 33:128
     characters{i} = zeros(9,7,3);
-    characters{i}(2:8,2:6,:) = double(round(1-(imread(i+".png")./255)));
+    characters{i}(2:8,2:6,:) = double(round(1-(imread("Caracteres\"+i+".png")./255)));
     characters{i} = characters{i}(:,:,1);
 end
 end
