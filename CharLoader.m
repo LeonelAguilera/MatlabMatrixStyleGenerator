d7a1b88f6e750cb5b5ac52f77c2307835a847f14
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-03-07 22:58:10 +0100

    Fixed dynamic allocating of "characters"
    Optimized loading chars speed.
    Previously, characters was given dimension (128-33=)95, then raised to 128 in "for i = 33:128".

--- CharLoader.m
+++ CharLoader.m
@@ -1,6 +1,6 @@
 function characters = CharLoader()
 % Loads all characters in folder Caracteres into a cell column vector
-characters = cell(1,128-33);
+characters = cell(1,128);
 for i = 33:128
     characters{i} = zeros(9,7,3);
     characters{i}(2:8,2:6,:) = double(round(1-(imread("Caracteres\"+i+".png")./255)));
