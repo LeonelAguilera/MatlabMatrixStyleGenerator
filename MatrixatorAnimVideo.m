d9b2722c50a891cf46ba2f5e7dff287c7d30996c
LeonelAguilera <64066370+LeonelAguilera@users.noreply.github.com>
2021-03-08 00:33:13 +0100

    Add files via upload

--- MatrixatorAnimVideo.m
+++ MatrixatorAnimVideo.m
@@ -1,5 +1,11 @@
 function output = MatrixatorAnimVideo(ResX,ResY,TrailLen,colorA,colorB,varargin)
 % pause(5)
+%{
+Por si no es evidente, este código ha sido hecho para cumplir con su
+objetivo de forma fácil y rápida, mantenerlo será horrible, más ineficiente
+no puede ser y no está comentado ni el nombre, a lo mejor esto cambia, pero
+no tiene pinta
+%}
 colorA = rgb2hsv(colorA);
 colorB = rgb2hsv(colorB);
 step = 1/TrailLen;
@@ -79,7 +85,7 @@ imshow(output)
 pause(0.1);
 imwrite(output,"Frame_0001.png");
 
-for frame = 2:200
+for frame = 2:72
     lineStart = Index*step-frame*step;
     lineEnd   = lineStart + step*(ResY-1);
     
@@ -129,6 +135,75 @@ for frame = 2:200
     
     output(:,:,1) = imresize(kron(H,ones(9,7)),size(logo),'nearest');%.*whitemap+(220/360).*(1-whitemap);
     output(:,:,2) = imresize(kron(S,ones(9,7)),size(logo),'nearest');%.*(1-whitemap);
+    output(:,:,3) = V;
+    
+    output = hsv2rgb(output);
+    subplot(1,1,1);
+    imshow(output)
+    if frame < 10
+        name = "Frame_000"+frame+".png";
+    elseif frame < 100
+        name = "Frame_00"+frame+".png";
+    elseif frame < 1000
+        name = "Frame_0"+frame+".png";
+    else
+        name = "Frame_"+frame+".png";
+    end
+    imwrite(output,name);
+    pause(0.1);
+end
+
+for frame = 73:(72+48) %72+48=120
+    lineStart = Index*step-frame*step;
+    lineEnd   = lineStart + step*(ResY-1);
+    
+    for x = 1:ResX
+        ShadowMap(:,x) = (lineStart(x):step:lineEnd(x))';
+    end
+    ShadowMap = ShadowMap-floor(ShadowMap);
+    ShadowMap(coordinates>(Index+frame)) = 0;
+    
+    
+    letters(2:ResY,:) = letters(1:ResY-1,:);
+    letters(1,:) = round((126-33)*rand(1,ResX))+33;
+    
+    for i=1:(nargin-5)
+        letters(mod(easterEggs{i,3}+frame,ResY)+1,easterEggs{i,2}+1:easterEggs{i,2}+size(easterEggs{i,1},2)) = easterEggs{i,1};
+        ShadowMap(mod(easterEggs{i,3}+frame,ResY)+1,easterEggs{i,2}+1:easterEggs{i,2}+size(easterEggs{i,1},2)) = min(ShadowMap(mod(easterEggs{i,3}+frame,ResY)+1,easterEggs{i,2}+1:easterEggs{i,2}+size(easterEggs{i,1},2)),1);
+    end
+    
+    %     ShadowMap = ShadowMap.*logo;
+    
+    CharMap = [characters{letters(1,:)}];
+    for y = 2:ResY
+        CharMap = [CharMap;[characters{letters(y,:)}]];
+    end
+    
+    H(2:ResY,:) = H(1:ResY-1,:);
+    H(1,:) = rand(1,ResX)*(colorB(1)-colorA(1))+colorA(1);
+    S(2:ResY,:) = S(1:ResY-1,:);
+    S(1,:) = rand(1,ResX)*(colorB(2)-colorA(2))+colorA(2);
+    V = kron(ShadowMap,ones(9,7));
+    V = V.*CharMap;
+    
+    %     H = imresize(H,size(logo));
+    %     S = imresize(S,size(logo));
+    V = imresize(V,size(internet));
+    
+    %     V = V.*logo;
+    
+    pre = zeros(size(V));
+    pre(V>((1/(frame-72))*((1/(frame-72))>0.1)))=1;
+    whitemap = max(whitemap - pre.*(1-internet),0);
+    whitemap = whitemap.*logo;
+%         subplot(1,2,2);
+%         imshow(whitemap)
+%         V = V.*logo;
+%     V = min(V+(1-internet).*(1-whitemap),1);
+    V = min(V+(1-whitemap),1);
+%     V = V.*logo;
+    output(:,:,1) = imresize(kron(H,ones(9,7)),size(internet),'nearest');%.*whitemap+(220/360).*(1-whitemap);
+    output(:,:,2) = imresize(kron(S,ones(9,7)),size(internet),'nearest');%.*(1-whitemap);
     output(:,:,3) = V;
     
     output = hsv2rgb(output);
