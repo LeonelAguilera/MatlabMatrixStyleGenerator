3adad141dd1dea05c235a6407ac78379f7f3fa20
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-03-09 17:04:50 +0100

    Fix characters folder

--- CharLoader.m
+++ CharLoader.m
@@ -3,7 +3,7 @@ characters = cell(1,128-33);
 characters{32} = zeros(9,7);
 for i = 33:128
     characters{i} = zeros(9,7,3);
-    characters{i}(2:8,2:6,:) = double(round(1-(imread(i+".png")./255)));
+    characters{i}(2:8,2:6,:) = double(round(1-(imread("Caracteres\"+i+".png")./255)));
     characters{i} = characters{i}(:,:,1);
 end
 end
